a6b45fad727291c49d47ab2ac3b7f512cc3876f0
Seema Taneja <seemamehta001@gmail.com>
2022-06-17 10:57:08 +0100

    Made some changes on Login page

--- myface-ui/src/Components/LoginManager/LoginManager.tsx
+++ myface-ui/src/Components/LoginManager/LoginManager.tsx
@@ -1,37 +1,56 @@
-﻿import React, {createContext, ReactNode, useState} from "react";
-
-export const LoginContext = createContext({
-    isLoggedIn: false,
-    isAdmin: false,
-    logIn: () => {},
-    logOut: () => {},
+﻿import React, { createContext, ReactNode, useState } from "react";
+interface LoginContextType {
+  isLoggedIn: boolean;
+  isAdmin: boolean;
+  logIn(): void;
+  logOut(): void;
+  username: string;
+  setUsername(args0: string): void;
+  password: string;
+  setPassword(args0: string): void;
+}
+export const LoginContext = createContext<LoginContextType>({
+  isLoggedIn: false,
+  isAdmin: false,
+  logIn: () => {},
+  logOut: () => {},
+  username: "",
+  setUsername: () => {},
+  password: "",
+  setPassword: () => {},
 });
 
 interface LoginManagerProps {
-    children: ReactNode
+  children: ReactNode;
 }
 
 export function LoginManager(props: LoginManagerProps): JSX.Element {
-    const [loggedIn, setLoggedIn] = useState(true);
-    
-    function logIn() {
-        setLoggedIn(true);
-    }
-    
-    function logOut() {
-        setLoggedIn(false);
-    }
-    
-    const context = {
-        isLoggedIn: loggedIn,
-        isAdmin: loggedIn,
-        logIn: logIn,
-        logOut: logOut,
-    };
-    
-    return (
-        <LoginContext.Provider value={context}>
-            {props.children}
-        </LoginContext.Provider>
-    );
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function logIn() {
+    setLoggedIn(true);
+  }
+
+  function logOut() {
+    setLoggedIn(false);
+  }
+
+  const context = {
+    isLoggedIn: loggedIn,
+    isAdmin: loggedIn,
+    logIn: logIn,
+    logOut: logOut,
+    username: username,
+    setUsername: setUsername,
+    password: password,
+    setPassword: setPassword,
+  };
+
+  return (
+    <LoginContext.Provider value={context}>
+      {props.children}
+    </LoginContext.Provider>
+  );
 }
